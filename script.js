e4083fa8cdc7da482c795e44092e2566ac46dcea
dack <dack@penguin>
2026-01-15 01:13:34 -0800

    Reworked visuals entirely, added .gitignore, changed game size

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const gravity_url = 'sounds/gravity.mp3';
 const powerup_url = 'sounds/powerup.mp3';
 const win_url = 'sounds/win.mp3';
 
+
 const gameover_sfx = new Audio(gameover_url);
 const gravity_sfx = new Audio(gravity_url);
 const powerup_sfx = new Audio(powerup_url);
@@ -124,36 +125,99 @@ class gameBoard{
 	}
 
 	draw_menu(){
+		// Draw dark gradient background
+		let gradient = ctx.createLinearGradient(0, 0, 0, height);
+		gradient.addColorStop(0, '#0a0a1a');
+		gradient.addColorStop(1, '#1a1a3a');
+		ctx.fillStyle = gradient;
+		ctx.fillRect(0, 0, width, height);
+
 		ctx.save();
 		ctx.translate(width/2, height/2+25);
-		ctx.fillStyle = '#CBB';
-		draw_rect({x:-50,y:-100},{x:50,y:-50}, true);
 
-		ctx.font = "bold 50px Roboto";
-		ctx.fillStyle = 'yellow';
-		ctx.strokeStyle = 'red';
-		ctx.fillText("Rebound", -100, -150);
-		ctx.strokeText("Rebound", -100, -150);
-		ctx.fillStyle = "black";
-		ctx.font = '40px Roboto';
-		ctx.fillText("Start",-43,-62);
-		ctx.font = "bold 25px Roboto";
-		ctx.fillText("How to Play", -65, -5);
-		ctx.fillStyle = 'black';
-		ctx.font = '18px Roboto';
-		ctx.translate(0,15);
-		ctx.fillText("Click and drag to launch a ball", -120,20);
-		ctx.fillText("Bounce the ball off walls to increase their value", -185,45);
-		ctx.fillText("Hit bonus hexagons for special effects", -150,70);
-		ctx.fillText("Pass through the hoops to score",-130,95);
-		ctx.font = 'italic 17px Roboto';
-		ctx.fillText("Multiply ball value by 2", -280,-60);
-		ctx.fillText('Remove gravity for 1.5 sec', 90, -60);
+		// Draw start button with neon glow
+		ctx.save();
+		ctx.shadowColor = '#00ffff';
+		ctx.shadowBlur = 20;
+		ctx.fillStyle = '#1a1a3a';
+		ctx.strokeStyle = '#00ffff';
+		ctx.lineWidth = 2;
+		ctx.beginPath();
+		ctx.roundRect(-60, -105, 120, 55, 8);
+		ctx.fill();
+		ctx.stroke();
+		ctx.restore();
+
+		// Title with horizontal shift effect and neon glow
+		ctx.save();
+		ctx.textAlign = 'center';
+		ctx.font = "900 42px 'Press Start 2P', monospace";
+
+		// Left shifted copy (lower opacity)
+		ctx.shadowBlur = 0;
+		ctx.fillStyle = 'rgba(255, 0, 255, 0.3)';
+		ctx.fillText("REBOUND", -8, -150);
+
+		// Right shifted copy (lower opacity)
+		ctx.fillStyle = 'rgba(0, 255, 255, 0.3)';
+		ctx.fillText("REBOUND", 8, -150);
+
+		// Main title
+		ctx.shadowColor = '#ff00ff';
+		ctx.shadowBlur = 30;
+		ctx.fillStyle = '#ff00ff';
+		ctx.fillText("REBOUND", 0, -150);
+		ctx.restore();
+
+		// Start button text
+		ctx.save();
+		ctx.textAlign = 'center';
+		ctx.shadowColor = '#00ffff';
+		ctx.shadowBlur = 10;
+		ctx.fillStyle = '#00ffff';
+		ctx.font = "700 28px 'Orbitron', sans-serif";
+		ctx.fillText("START", 0, -68);
+		ctx.restore();
+
+		// Bonus icons positioned between button and instructions
+		let mult_bonus = new Bonus(this, 'mult', {x: width/2 - 130, y: height/2 + 5} );
+		let grav_bonus = new Bonus(this, 'grav', {x: width/2 + 130, y: height/2 + 5} );
+
+		// Bonus descriptions (centered under icons)
+		ctx.save();
+		ctx.textAlign = 'center';
+		ctx.font = "400 11px 'Orbitron', sans-serif";
+		ctx.fillStyle = '#ffff00';
+		ctx.fillText("2x Multiplier", -130, 25);
+		ctx.fillStyle = '#00ff88';
+		ctx.fillText("Zero Gravity", 130, 25);
+		ctx.restore();
+
+		// How to Play header (centered)
+		ctx.save();
+		ctx.textAlign = 'center';
+		ctx.shadowColor = '#00ff88';
+		ctx.shadowBlur = 10;
+		ctx.fillStyle = '#00ff88';
+		ctx.font = "700 16px 'Orbitron', sans-serif";
+		ctx.fillText("HOW TO PLAY", 0, 55);
+		ctx.restore();
+
+		// Instructions text (centered)
+		ctx.save();
+		ctx.textAlign = 'center';
+		ctx.fillStyle = '#8888aa';
+		ctx.font = "400 12px 'Orbitron', sans-serif";
+		ctx.fillText("Click and drag to launch a ball", 0, 80);
+		ctx.fillText("Bounce off walls to increase value", 0, 98);
+		ctx.fillText("Hit hexagons for special effects", 0, 116);
+		ctx.fillText("Pass through hoops to score", 0, 134);
+		ctx.restore();
 
 		ctx.restore();
-		let mult_bonus = new Bonus(this, 'mult', {x:-200, y: -100} );
+
+		// Draw the bonus hexagons (using absolute coordinates)
 		mult_bonus.draw();
-		let grav_bonus = new Bonus(this, 'grav', {x:180, y: -100} );
 		grav_bonus.draw();
 	}
 	mouse_down(e){
@@ -162,8 +226,8 @@ class gameBoard{
 			this.startpos = {x: e.x, y: e.y};
 		}
 		else{
-			if(e.x > 250 && e.x < 350 && e.y > 135 && e.y < 185  ){
-				//If you click  within the start button area
+			if(e.x > 240 && e.x < 360 && e.y > 120 && e.y < 175  ){
+				//If you click within the start button area
 				this.start_game();
 			}
 		}
@@ -179,7 +243,6 @@ class gameBoard{
 	draw_sling(){
 		if(this.moved){
 			ctx.save();
-			ctx.lineWidth = 2;
 			let delta_x = this.startpos.x - this.pos.x;
 			let delta_y = this.startpos.y - this.pos.y;
 			let angle = Math.atan2(delta_y, delta_x);
@@ -196,12 +259,21 @@ class gameBoard{
 				y: this.startpos.y - delta_y_clamped
 			};
 
+			// Draw ball preview with glow
 			ctx.save();
-			ctx.fillStyle = 'black';
+			ctx.shadowColor = '#ff4488';
+			ctx.shadowBlur = 15;
+			ctx.fillStyle = '#ff4488';
+			ctx.beginPath();
 			ctx.arc(this.ball_startpos.x,this.ball_startpos.y,this.ball_size,0,Math.PI*2);
 			ctx.fill();
 			ctx.restore();
 
+			// Draw slingshot lines with neon effect
+			ctx.strokeStyle = '#00ffff';
+			ctx.shadowColor = '#00ffff';
+			ctx.shadowBlur = 10;
+			ctx.lineWidth = 3;
 			ctx.beginPath();
 			ctx.translate(this.startpos.x, this.startpos.y);
 			ctx.rotate(angle);
@@ -210,28 +282,36 @@ class gameBoard{
 			ctx.lineTo(-distance_clamped,0);
 			ctx.moveTo(0, spread);
 			ctx.lineTo(-distance_clamped,0);
-			ctx.closePath();
 			ctx.stroke();
-			
+
 			ctx.restore();
 		}
 	}
 	draw_lives(){
 		ctx.save();
-		ctx.translate(30,30);
-		ctx.fillStyle = 'black';
+		ctx.translate(25, 25);
 		let y = 0;
-		let x = -35;
+		let x = -25;
 		for(let i=0; i<this.lives; i++){
 			if(i%5===0 && i > 0 ){
-				y += 35;
-				x -= (35*5);
+				y += 28;
+				x -= (28*5);
 			}
-			x += 35;
+			x += 28;
+			ctx.save();
+			ctx.shadowColor = '#ff4488';
+			ctx.shadowBlur = 8;
+			ctx.fillStyle = '#ff4488';
 			ctx.beginPath();
-			ctx.arc(x,y,12,0,Math.PI*2);
+			ctx.arc(x, y, 10, 0, Math.PI*2);
 			ctx.fill();
 			ctx.closePath();
+			// Inner highlight
+			ctx.fillStyle = '#ffaacc';
+			ctx.beginPath();
+			ctx.arc(x-2, y-2, 4, 0, Math.PI*2);
+			ctx.fill();
+			ctx.restore();
 		}
 		ctx.restore();
 	}
@@ -274,7 +354,7 @@ class gameBoard{
 			if(this.score%5 === 0){
 				if(this.lives >= 15){
 					this.score +=5;
-					let text = new FloatingText("Max Lives Bonus!", 25, {x: width/2-75, y: height/5});
+					let text = new FloatingText("MAX LIVES!", 25, {x: width/2-60, y: height/5}, {x:0,y:-2}, {r:255, g:0, b:255});
 					this.texts.push(text);
 				}
 				else{
@@ -283,11 +363,10 @@ class gameBoard{
 			}
 		}
 		if( score > 0){
-			text = new FloatingText("+"+score, 30, pos);
-			
+			text = new FloatingText("+"+score, 30, pos, undefined, {r:0, g:255, b:255});
 		}
 		else{
-			text = new FloatingText("No score!",20, pos);
+			text = new FloatingText("MISS!", 20, pos, undefined, {r:255, g:100, b:100});
 		}
 		this.texts.push(text);
 		if(this.score >= this.target){
@@ -320,16 +399,16 @@ class gameBoard{
 				
 			}, this.lives*500+1000);
 			let text_options = [
-				"Epic!",
-				"Nice!",
-				"Great!",
-				"Cool!",
-				"Slick!",
-				"Wow!!"
+				"EPIC!",
+				"NICE!",
+				"GREAT!",
+				"COOL!",
+				"SLICK!",
+				"WOW!!"
 			]
 			let text = text_options[Math.floor(Math.random()*text_options.length)];
 			//Pick a random choice for round clear
-			let win_text = new FloatingText(text, 50, {x: width/2-60, y: height/2});
+			let win_text = new FloatingText(text, 45, {x: width/2-60, y: height/2}, {x:0, y:-1}, {r:0, g:255, b:255});
 			this.texts.push(win_text);
 			if(this.lives!= 0){
 				life_interval = setInterval(()=>{
@@ -352,7 +431,13 @@ class gameBoard{
 		
 	}
 	animate(){
-		ctx.clearRect(0,0,this.width,this.height);
+		// Draw dark gradient background
+		let gradient = ctx.createLinearGradient(0, 0, 0, height);
+		gradient.addColorStop(0, '#0a0a1a');
+		gradient.addColorStop(1, '#1a1a3a');
+		ctx.fillStyle = gradient;
+		ctx.fillRect(0, 0, width, height);
+
 		if(this.lives === 0 && this.balls.length === 0 && !this.in_menu && !this.game_end){
 			gameover_sfx.currentTime = 0;
 			gameover_sfx.volume = 0.5;
@@ -360,22 +445,36 @@ class gameBoard{
 			this.in_menu = true;
 			clearInterval(this.interval);
 			ctx.save();
-			ctx.font = "bold 40px Roboto";
-			ctx.fillText("You Lose!!!", this.width/2-100, this.height/2);
+			ctx.textAlign = 'center';
+			ctx.shadowColor = '#ff0044';
+			ctx.shadowBlur = 20;
+			ctx.fillStyle = '#ff0044';
+			ctx.font = "700 32px 'Orbitron', sans-serif";
+			ctx.fillText("GAME OVER", this.width/2, this.height/2);
 			ctx.restore();
 			setTimeout((()=>{
-				this.start_game();
-				console.log("restarting");
+				this.draw_menu();
 			}).bind(this) ,1500);
 			return;
 		}
+
+		// Draw score with neon effect
 		ctx.save();
-		ctx.fillStyle = "black";
-		ctx.translate(this.width/2-25, 50);
-		ctx.font = "bold 45px Roboto";
-		ctx.fillText(this.score,0,0);
-		ctx.font = "30px Roboto";
-		ctx.fillText("Target: "+this.target,125, -5);
+		ctx.translate(this.width/2, 45);
+		ctx.shadowColor = '#00ffff';
+		ctx.shadowBlur = 15;
+		ctx.fillStyle = '#00ffff';
+		ctx.font = "700 40px 'Orbitron', sans-serif";
+		ctx.textAlign = 'center';
+		ctx.fillText(this.score, 0, 0);
+
+		// Draw target
+		ctx.shadowColor = '#ff00ff';
+		ctx.shadowBlur = 10;
+		ctx.fillStyle = '#ff88ff';
+		ctx.font = "400 18px 'Orbitron', sans-serif";
+		ctx.textAlign = 'right';
+		ctx.fillText("TARGET: " + this.target, 270, -5);
 		ctx.restore();
 
 		if(this.drawing && this.lives > 0){
@@ -413,7 +512,7 @@ class gameBoard{
 						gravity_sfx.volume = 0.8;
                     	powerup_sfx.play().catch(e => console.error("Powerup playback error:", e));
 						ball.multiplier *= bonus.value;
-						let text = new FloatingText("x2",20,bonus.pos);
+						let text = new FloatingText("x2", 24, bonus.pos, undefined, {r:255, g:255, b:0});
 						this.texts.push(text);
 					}
 					else if(bonus.type === 'grav'){
@@ -430,7 +529,7 @@ class gameBoard{
 						ball.vel.x *= 1.04;
 						ball.vel.y *= 1.04;
 						ball.elasticity = 1;
-						let text = new FloatingText("Floating!", 20, bonus.pos, undefined, {r:50,g:220,b:50});
+						let text = new FloatingText("ZERO-G!", 24, bonus.pos, undefined, {r:0, g:255, b:136});
 						this.texts.push(text);
 
 						this.gravTimeout = setTimeout(() =>{
@@ -475,7 +574,7 @@ class gameBoard{
 }
 
 class FloatingText{
-	constructor(text, size, pos, vel={x:(Math.random()-0.5)*5,y:(Math.random()-0.5)*5}, colour = {r:255, g:200, b: 50}){
+	constructor(text, size, pos, vel={x:(Math.random()-0.5)*5,y:(Math.random()-0.5)*5}, colour = {r:0, g:255, b: 255}){
 		this.text = text;
 		this.pos = {x: pos.x, y: pos.y};
 		this.vel = {x: vel.x, y: vel.y};
@@ -488,35 +587,47 @@ class FloatingText{
 	}
 	draw(){
 		ctx.save();
-		ctx.fillStyle = `rgba(${this.colour.r},${this.colour.g},${this.colour.b},`+this.opacity+")";
-		ctx.font = "bold "+this.size+"px Roboto";
-		
-		ctx.fillText(this.text,this.pos.x-10,this.pos.y+15);
-		ctx.restore();	
-		this.opacity -= 0.01;
+		let colorStr = `rgba(${this.colour.r},${this.colour.g},${this.colour.b},${this.opacity})`;
+		ctx.shadowColor = colorStr;
+		ctx.shadowBlur = 15;
+		ctx.fillStyle = colorStr;
+		ctx.font = "700 "+this.size+"px 'Orbitron', sans-serif";
+		ctx.fillText(this.text, this.pos.x-10, this.pos.y+15);
+		ctx.restore();
+		this.opacity -= 0.015;
 		this.pos.x += (Math.random()-0.5)*0.05+this.vel.x*0.02;
 		this.pos.y += (Math.random()-0.5)*0.05+this.vel.y*0.02;
+		if(this.opacity <= 0){
+			this.active = false;
+		}
 	}
 }
 class Trail{
-	constructor(pos,radius,red=255){
+	constructor(pos, radius, red=255){
 		this.pos = {x: pos.x, y: pos.y};
 		this.active = true;
-		this.opacity = 1;
+		this.opacity = 0.8;
 		this.radius = radius;
-		this.green = 160;
-		this.red = red;
+		this.isGravity = red === 0; // Green trail for gravity powerup
 	}
 	draw(){
 		ctx.save();
-		ctx.fillStyle = "rgba("+this.red+","+this.green+",50,"+this.opacity+")";
+		let color;
+		if(this.isGravity){
+			color = `rgba(0, 255, 136, ${this.opacity})`;
+			ctx.shadowColor = '#00ff88';
+		} else {
+			color = `rgba(255, 68, 136, ${this.opacity})`;
+			ctx.shadowColor = '#ff4488';
+		}
+		ctx.shadowBlur = 10;
+		ctx.fillStyle = color;
 		ctx.beginPath();
-		ctx.arc(this.pos.x,this.pos.y,this.radius,0,Math.PI*2);
+		ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI*2);
 		ctx.closePath();
 		ctx.fill();
-		this.green-=5;
-		this.opacity -= 0.02;
-		this.radius *= 0.95;
+		this.opacity -= 0.025;
+		this.radius *= 0.92;
 
 		if(this.opacity <= 0 || this.radius < 0.5){
 			this.active = false;
@@ -536,8 +647,10 @@ class Bonus{
 		else{
 			this.type = Math.random() > 0.666 ? 'mult' : 'grav';
 		}
-		
-		this.colour = this.type === 'mult' ? 'rgb(220,220,0)' : 'rgb(50,200,50)';
+
+		// Neon colors for bonuses
+		this.colour = this.type === 'mult' ? '#ffff00' : '#00ff88';
+		this.glowColor = this.type === 'mult' ? '#ffff00' : '#00ff88';
 		if(pos){
 			this.pos = pos;
 		}
@@ -555,20 +668,41 @@ class Bonus{
 			}
 		}
 		this.rotation = Math.random()*Math.PI*2;
+		this.rotationSpeed = 0.02;
 		this.value = 2;
+		this.pulsePhase = Math.random() * Math.PI * 2;
 	}
 	draw(){
 		ctx.save();
-		ctx.fillStyle=this.colour;
-		ctx.translate(this.pos.x,this.pos.y);
+		ctx.translate(this.pos.x, this.pos.y);
+
+		// Pulsing glow effect
+		this.pulsePhase += 0.08;
+		let pulse = 0.5 + Math.sin(this.pulsePhase) * 0.3;
+		ctx.shadowColor = this.glowColor;
+		ctx.shadowBlur = 15 + pulse * 10;
+
+		ctx.fillStyle = this.colour;
 		ctx.beginPath();
 		ctx.rotate(this.rotation);
+		this.rotation += this.rotationSpeed;
 		for(let i=0; i<=5; i++){
-			ctx.lineTo(0,this.radius);
+			ctx.lineTo(0, this.radius);
 			ctx.rotate(Math.PI/180*60);
 		}
 		ctx.closePath();
 		ctx.fill();
+
+		// Inner glow
+		ctx.fillStyle = 'rgba(255,255,255,0.3)';
+		ctx.beginPath();
+		for(let i=0; i<=5; i++){
+			ctx.lineTo(0, this.radius * 0.5);
+			ctx.rotate(Math.PI/180*60);
+		}
+		ctx.closePath();
+		ctx.fill();
+
 		ctx.restore();
 	}
 }
@@ -578,6 +712,7 @@ class Basket{
 		this.active = true;
 		this.size = 55;
 		this.board = gameBoard;
+		this.pulsePhase = Math.random() * Math.PI * 2;
 		//75 pixel offset from walls, -30 is half the width of the basket, -5 is half the height of basket
 		this.pos = {x: Math.random()*(width-150)+75-30, y: Math.random()*(height-150)+75-5};
 		for(let i=0; i<this.board.bonuses.length; i++){
@@ -590,18 +725,35 @@ class Basket{
 					distance = get_distance(bonus.pos, this.pos);
 			}
 		}
-		this.coords = {x1: this.pos.x,y1: this.pos.y,x2: this.pos.x+this.size,y2: this.pos.y+5};
+		this.coords = {x1: this.pos.x, y1: this.pos.y, x2: this.pos.x+this.size, y2: this.pos.y+5};
 	}
 	draw(){
 		ctx.save();
-		ctx.fillStyle = 'red';
-		ctx.fillRect(this.pos.x,this.pos.y,this.size,5);
+
+		// Pulsing glow effect
+		this.pulsePhase += 0.06;
+		let pulse = 0.5 + Math.sin(this.pulsePhase) * 0.3;
+
+		ctx.shadowColor = '#ff00ff';
+		ctx.shadowBlur = 12 + pulse * 8;
+
+		// Create gradient for hoop
+		let gradient = ctx.createLinearGradient(this.pos.x, this.pos.y, this.pos.x + this.size, this.pos.y);
+		gradient.addColorStop(0, '#ff00ff');
+		gradient.addColorStop(0.5, '#ff88ff');
+		gradient.addColorStop(1, '#ff00ff');
+
+		ctx.fillStyle = gradient;
+		ctx.beginPath();
+		ctx.roundRect(this.pos.x, this.pos.y, this.size, 6, 3);
+		ctx.fill();
+
 		ctx.restore();
 	}
 }
 
 class Ball{
-	constructor(pos,vel, radius=5){
+	constructor(pos, vel, radius=5){
 		this.active = true;
 		this.colliding = false;
 		this.radius = radius;
@@ -609,7 +761,7 @@ class Ball{
 		this.elasticity = 0.96;
 		this.friction = {x: 0.997, y: 0.995};
 		this.grav_power = false;
-		this.red = 0;
+		this.hue = 330; // Start at pink/magenta
 		this.pos = pos;
 		this.prevpos = pos;
 		this.vel = vel;
@@ -642,13 +794,13 @@ class Ball{
 		this.vel.y *= this.elasticity;
 		if(Math.abs(this.vel.y) > 5){
 			this.bounces++;
-			let bounceText = new FloatingText('+1', 14, this.pos, this.vel);
+			let bounceText = new FloatingText('+1', 14, this.pos, this.vel, {r:0, g:255, b:255});
 			board.texts.push(bounceText);
 		}
-		if(this.red < 100){
-			this.red +=5;	
+		// Shift hue towards cyan as bounces increase
+		if(this.hue > 180){
+			this.hue -= 10;
 		}
-		
 	}
 	handleCollision(){
 		//Make the ball move directly next to the wall
@@ -676,11 +828,25 @@ class Ball{
 	}
 	draw(){
 		ctx.save();
+
+		// Neon glow ball
+		let color = this.grav_power ? '#00ff88' : `hsl(${this.hue}, 100%, 60%)`;
+		let glowColor = this.grav_power ? '#00ff88' : `hsl(${this.hue}, 100%, 50%)`;
+
+		ctx.shadowColor = glowColor;
+		ctx.shadowBlur = 15;
+		ctx.fillStyle = color;
 		ctx.beginPath();
-		ctx.fillStyle = "rgba("+this.red+",0,0)";
-		ctx.arc(this.pos.x,this.pos.y,this.radius,0,Math.PI*2);
+		ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI*2);
 		ctx.closePath();
 		ctx.fill();
+
+		// Inner highlight
+		ctx.fillStyle = 'rgba(255,255,255,0.4)';
+		ctx.beginPath();
+		ctx.arc(this.pos.x - this.radius*0.3, this.pos.y - this.radius*0.3, this.radius*0.4, 0, Math.PI*2);
+		ctx.fill();
+
 		ctx.restore();
 	}
 }
@@ -688,8 +854,16 @@ class Ball{
 
 let board = new gameBoard(width,height);
 
+// Helper to get canvas-relative coordinates
+function getCanvasPos(e) {
+	let rect = canvas.getBoundingClientRect();
+	return {
+		x: e.clientX - rect.left,
+		y: e.clientY - rect.top
+	};
+}
 
-canvas.addEventListener('pointerdown', (e) => board.mouse_down(e));
-document.addEventListener('pointermove', (e) => board.set_pos(e));
-document.addEventListener('pointerup', (e) => board.shoot(e));
+canvas.addEventListener('pointerdown', (e) => board.mouse_down(getCanvasPos(e)));
+document.addEventListener('pointermove', (e) => board.set_pos(getCanvasPos(e)));
+document.addEventListener('pointerup', (e) => board.shoot(getCanvasPos(e)));
 //Allows you to shoot even if you drag off the canvas
